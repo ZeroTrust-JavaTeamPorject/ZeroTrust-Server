144d9b2dfa4a4869d384ed88719dca3fa80628f6
이재현 <2023050@bssm.hs.kr>
2025-01-06 19:43:37 +0900

    fix(login): 로그인 실패 시 세션 전달 안하게 로직 수정

--- src/main/java/com/example/zerotrust/global/config/SecurityConfig.java
+++ src/main/java/com/example/zerotrust/global/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.zerotrust.global.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -7,11 +10,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import java.io.IOException;
 
 @Slf4j
 @Configuration
@@ -22,16 +30,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf(csrf -> csrf.csrfTokenRepository(csrfTokenRepository())
-//                .ignoringRequestMatchers("/**")
-//        );
         http.csrf(csrf -> csrf.disable());
+        http.sessionManagement((session) ->
+                session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+        );
         http.authorizeHttpRequests((authorize) ->
                 authorize.requestMatchers("/**").permitAll()
         );
         http.formLogin((formLogin) -> formLogin.loginPage("/login")
                 .defaultSuccessUrl("/otp")
-                .failureUrl("/fail")
+                .failureHandler(new AuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                        // 로그인 실패 시 세션 생성 안 함
+                        request.getSession(false); // 기존 세션이 있으면 가져오지만 새 세션은 생성하지 않음
+                        response.sendRedirect("/fail");
+                    }
+                })
         );
         http.logout(logout -> logout.logoutUrl("/logout"));
 
